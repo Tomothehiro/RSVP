5ba09ac55c00ff0b01af6c601f5662ae64796f7a
tomo <tomonouchi@hotmail.com>
2017-10-27 14:58:55 -0700

    added user role

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     AUTH0_DOMAIN: 'tomohiro-sato.auth0.com', // e.g., kmaida.auth0.com
     AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://tomohiro_sato:meanrsvp@ds036577.mlab.com:36577/mean-rsvp'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://tomohiro_sato:meanrsvp@ds036577.mlab.com:36577/mean-rsvp',
+    NAMESPACE: 'http://myapp.com/roles'
 };
