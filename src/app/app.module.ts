3dba3bc23ea4fa820a9d08bbba3536311520057d
tomo <tomonouchi@hotmail.com>
2017-10-31 19:39:14 -0700

    refactored NgModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,69 +1,35 @@
-import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { DatePipe } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ApiService } from './core/api.service';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { EventModule } from './pages/event/event.module';
+import { AdminModule } from './pages/admin/admin.module';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthService } from './auth/auth.service';
-import { FilterSortService } from './core/filter-sort.service';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { CallbackComponent } from './pages/callback/callback.component';
-import { LoadingComponent } from './core/loading.component';
-import { UtilsService } from './core/utils.service';
-import { AdminComponent } from './pages/admin/admin.component';
-import { EventComponent } from './pages/event/event.component';
-import { EventDetailComponent } from './pages/event/event-detail/event-detail.component';
-import { RsvpComponent } from './pages/event/rsvp/rsvp.component';
-import { RsvpFormComponent } from './pages/event/rsvp/rsvp-form/rsvp-form.component';
-import { SubmittingComponent } from './core/forms/submitting.component';
-import { CreateEventComponent } from './pages/admin/create-event/create-event.component';
-import { UpdateEventComponent } from './pages/admin/update-event/update-event.component';
-import { EventFormComponent } from './pages/admin/event-form/event-form.component';
-import { DeleteEventComponent } from './pages/admin/update-event/delete-event/delete-event.component';
 import { MyRsvpsComponent } from './pages/my-rsvps/my-rsvps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    FooterComponent,
     CallbackComponent,
-    LoadingComponent,
-    AdminComponent,
-    EventComponent,
-    EventDetailComponent,
-    RsvpComponent,
-    RsvpFormComponent,
-    SubmittingComponent,
-    CreateEventComponent,
-    UpdateEventComponent,
-    EventFormComponent,
-    DeleteEventComponent,
     MyRsvpsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AuthModule,
+    CoreModule,
+    EventModule,
+    AdminModule
   ],
-  providers: [
-    Title,
-    AuthService,
-    ApiService,
-    DatePipe,
-    UtilsService,
-    FilterSortService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/admin/admin.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoreModule } from './../../core/core.module';
+import { AdminComponent } from './admin.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { UpdateEventComponent } from './update-event/update-event.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { DeleteEventComponent } from './update-event/delete-event/delete-event.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoreModule
+  ],
+  declarations: [
+    AdminComponent,
+    CreateEventComponent,
+    UpdateEventComponent,
+    EventFormComponent,
+    DeleteEventComponent
+  ]
+})
+export class AdminModule { }
